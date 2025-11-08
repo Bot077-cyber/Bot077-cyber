32ba90dbddd0477c03e32120d7287560ac6309da
Bot077-cyber <saeemsheikh2006@gmail.com>
2025-11-08 14:39:42 +0600

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - আসসালামুয়ালাইকুম আপনি নিকনেম চেঞ্জ করতে পারবেন না🥀\n শুধু আমার বস ছাইম শেখ চেঞ্জ করতে পারবে🖐`}, threadID);
     }  
         }
